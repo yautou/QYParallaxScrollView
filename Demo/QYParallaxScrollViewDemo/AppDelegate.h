d33d77b6b347da04a0ce4327433504d6605502d6
qeye <qeye@mgtv.com>
2018-02-22 14:58:49 +0800

    Add demo project

---
+++ Demo/QYParallaxScrollViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYParallaxScrollViewDemo
+//
+//  Created by Qeye Wang on 22/02/2018.
+//  Copyright © 2018 Qeye Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/QYParallaxScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYParallaxScrollViewDemo
+//
+//  Created by Qeye Wang on 22/02/2018.
+//  Copyright © 2018 Qeye Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
